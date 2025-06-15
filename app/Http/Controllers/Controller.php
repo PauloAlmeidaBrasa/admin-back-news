dc32b7599c7e70e653d7a4ee924bb97144823a4c
pauloProgg <alemida.desenvolve@gmail.com>
2025-06-15 15:42:06 -0300

    feat: getting users by client id

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,13 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    static function respondWithSucess($data,$feat) {
+        return response()->json([
+            'success' => true,
+            'data' => [
+                $feat => $data
+            ]
+        ],200);
+    }
 }
